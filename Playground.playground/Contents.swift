fcd7e15231c47070f9706bb6340296776b2a0d17
Yalda <yalda.saeedi1381@gmail.com>
2023-08-14 22:58:11 +0330

    perf and feat the Exercises of day2

--- Playground.playground/Contents.swift
+++ Playground.playground/Contents.swift
@@ -1,15 +1,17 @@
-let studentInfo1: [String: Any] = ["name": "yalda", "age": 20, "totalScore": 18.56]
-let studentInfo2: [String: Any] = ["name": "sara", "age": 23, "totalScore": 17.02]
-let studentInfo3: [String: Any] = ["name": "sina", "age": 21, "totalScore": 16.66]
-let studentInfo4: [String: Any] = ["name": "nima", "age": 22, "totalScore": 17.34]
-let studentInfo5: [String: Any] = ["name": "bahar", "age": 24, "totalScore": 14.32]
-let studentInfo6: [String: Any] = ["name": "zeynab", "age": 25, "totalScore": 15.78]
-
+//using dictionary
+let studentInfo1: [String: Any] = ["name": "yalda", "age": 20, "totalScore": 18.56, "studentID" : 1] 
+let studentInfo2: [String: Any] = ["name": "sara", "age": 23, "totalScore": 17.02, "studentID" : 2]
+let studentInfo3: [String: Any] = ["name": "sina", "age": 21, "totalScore": 16.66, "studentID" : 3]
+let studentInfo4: [String: Any] = ["name": "nima", "age": 22, "totalScore": 17.34, "studentID" : 4]
+let studentInfo5: [String: Any] = ["name": "bahar", "age": 24, "totalScore": 14.32, "studentID" : 5]
+let studentInfo6: [String: Any] = ["name": "zeynab", "age": 25, "totalScore": 15.78, "studentID" : 6]
+//using arrey
 var allStudent = [studentInfo1, studentInfo2, studentInfo3, studentInfo4, studentInfo5, studentInfo6]
 var scores: [Double] = []
 let num = 1...6
 var student: [String: Any] = [:]
-
+//using set
+var studentsID = Set<Int>() 
 for i in num {
     student = allStudent[i - 1]
     if let totalScore = student["totalScore"] as? Double {
@@ -17,6 +19,11 @@ for i in num {
     } else {
         print("Total Score is not available.")
     }
+    if let ID = student["studentID"] as? Int {
+        studentsID.insert(ID)
+    } else {
+        print("Total Score is not available.")
+    }
 }
-
+print(studentsID)
 print(scores)
