7642525e4ffaf4f7c717b46788a91b11a6244c5f
yaldasaeedi <125181431+yaldasaeedi@users.noreply.github.com>
2023-08-16 10:00:19 +0330

    Update Contents.swift
    feat .

--- Playground.playground/Contents.swift
+++ Playground.playground/Contents.swift
@@ -1,21 +1,22 @@
+//: Playground - noun: a place where people can play
 //using enum
 enum allMajor {
-  case computer_engineering
-  case electrical_engineering
-  case biomedical_engineering
-  case mechanical_engineering
-  case french_literature
-  case psychology
-  case architecture
-  case painting
+    case computer_engineering
+    case electrical_engineering
+    case biomedical_engineering
+    case mechanical_engineering
+    case french_literature
+    case psychology
+    case architecture
+    case painting
 }
 struct student {
-  var name : String
-  var age : Int
-  var totalScore : Double
-  let studentID : Int
-  let major : allMajor
-
+    var name : String
+    var age : Int
+    var totalScore : Double
+    let studentID : Int
+    let major : allMajor
+    
 }
 let studentInfo1 = student(name: "yalda", age: 20, totalScore: 17.20,  studentID: 1, major: allMajor.computer_engineering)
 let studentInfo2 = student(name: "sara", age: 23, totalScore:  17.02,  studentID: 2, major: allMajor.biomedical_engineering)
@@ -25,108 +26,111 @@ let studentInfo5 = student(name: "bahar", age: 24, totalScore: 14.56,  studentID
 let studentInfo6 = student(name: "zeynab", age: 25, totalScore: 18.56,  studentID: 6, major: allMajor.french_literature)
 let studentInfo7 = student(name: "amin", age: 18, totalScore: 16.6,  studentID: 7, major: allMajor.mechanical_engineering)
 
-//using arrey
+var update : (() -> Void) = {}
+var showAverage : (() -> Void) = {}
+var showStudentList: (() -> Void) = {}
+var sortByName: (() -> Void) = {}
+
 var allStudent = [
-  studentInfo1, studentInfo2, studentInfo3, studentInfo4, studentInfo5, studentInfo6,
+    studentInfo1, studentInfo2, studentInfo3, studentInfo4, studentInfo5, studentInfo6,
 ]
 var scores: [Double] = []
 var averageScore: Double = 0
 let num = 1...6
 //using set
 var studentsID = Set<Int>()
+print("hi ,welcome to my school manager!")
 
-var hi = { ()->Int in 
-
-  print("hi, welcome to my school manager!\nPlease enter the option you want:\n1: updating\n2: average of scores\n3: list of student\n4: adding a new student\n5: deleting a student\n 6:sort by name\n")
-  if let input =  readLine(){
-
-    if let number = Int(input) {
-
-        print("You entered: \(number)")
-
-        return number
+var hi = {()->Int in
+    print("\nPlease enter the option you want:\n1: updating\n2: average of scores\n3: list of student\n4: adding a new student\n5: deleting a student\n6: sort by name")
+    if let input =  readLine(){
+        
+        if let number = Int(input) {
+            
+            print("You entered: \(number)")
+            
+            return number
+        } else {
+            
+            print("Invalid input. Please enter a valid number.")
+            
+            return 0
+        }
     } else {
-
-        print("Invalid input. Please enter a valid number.")
-
+        print("No input provided.")
         return 0
     }
-  } else {
-    print("No input provided.")
-    return 0
-  }
+    
 }
 func goToOption (action1: () -> Void, action2: () -> Void, action3: () -> Void, action4: () -> Void){
-  var theOption : Int
-  theOption = hi()
-  switch theOption{
+    var theOption : Int
+    theOption = hi()
+    switch theOption{
+        
     case 1:
-      action1()
+        action1()
     case 2:
-      action2()
+        action2()
     case 3:
-      break
+        action3()
     case 4:
-      break
+        break
     case 5:
-      break
+        break
     case 6:
-      break
-    default: 
-      theOption = hi()
-  }
+        action4()
+    default:
+        theOption = hi()
+    }
 }
-var update : (() -> Void) = {}
-var showAverage : (() -> Void) = {}
-var showStudentList : (() -> Void) = {}
-var sortByName : (() -> Void) = {}
 
 showAverage = {
-
-  averageScore = averageScore / 6
-  if averageScore >= 17 {
-
-    print("your students are average is excellent")
-  } else if averageScore >= 14 && averageScore < 17 {
-
-    print("your students are average is good")
-  } else {
-
-    print("most of your student needs more exercise")
-  }
-  goToOption(action1: update, action2:showAverage, action3: showStudentList, action4:sortByName)
-}
-update = { 
-
-  var tempStudent : student
-  for i in num {
     
-      tempStudent = allStudent[i - 1]
-      scores.append(tempStudent.totalScore)
-      averageScore += tempStudent.totalScore
-      studentsID.insert(tempStudent.studentID)
-  }
-  goToOption(action1: update, action2:showAverage, action3: showStudentList, action4:sortByName)
+    averageScore = averageScore / 6
+    if averageScore >= 17 {
+        
+        print("your students are average is excellent")
+    } else if averageScore >= 14 && averageScore < 17 {
+        
+        print("your students are average is good")
+    } else {
+        
+        print("most of your student needs more exercise")
+    }
+    goToOption(action1: update, action2:showAverage, action3: showStudentList, action4:sortByName)
+}
+update = {
+    print("im in updating")
+    var tempStudent : student
+    for i in num {
+        print("im in updating")
+        tempStudent = allStudent[i - 1]
+        scores.append(tempStudent.totalScore)
+        averageScore += tempStudent.totalScore
+        studentsID.insert(tempStudent.studentID)
+    }
+    goToOption(action1: update, action2:showAverage, action3: showStudentList, action4:sortByName)
 }
 showStudentList = {
-
-  for student in allStudent {
-
-    print("Name: \(student.name)")
-    print("Age: \(student.age)")
-    print("Total Score: \(student.totalScore)")
-    print("Student ID: \(student.studentID)")
-    print("Major: \(student.major)")
-    print("-----------------------------")
-  }
-  goToOption(action1: update, action2:showAverage, action3: showStudentList, action4:sortByName)
+    
+    for student in allStudent {
+        
+        print("Name: \(student.name)")
+        print("Age: \(student.age)")
+        print("Total Score: \(student.totalScore)")
+        print("Student ID: \(student.studentID)")
+        print("Major: \(student.major)")
+        print("-----------------------------")
+    }
+    goToOption(action1: update, action2:showAverage, action3: showStudentList, action4:sortByName)
 }
 sortByName = {
-
-  var allName = [studentInfo1.name, studentInfo2.name, studentInfo3.name, studentInfo4.name,  studentInfo5.name,  studentInfo6.name,  studentInfo7.name ]
-  allName.sort()
-  print ("allName")
-  goToOption(action1: update, action2:showAverage, action3: showStudentList, action4:sortByName)
+    
+    var allName = [studentInfo1.name, studentInfo2.name, studentInfo3.name, studentInfo4.name,  studentInfo5.name,  studentInfo6.name,  studentInfo7.name ]
+    allName.sort()
+    print (allName)
+    goToOption(action1: update, action2:showAverage, action3: showStudentList, action4:sortByName)
 }
+goToOption(action1: update, action2:showAverage, action3: showStudentList, action4:sortByName)
 let deletingStudent = {}
 let addingStudent = {}
